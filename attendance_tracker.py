bb3e7ee29ede6a219568a4ddda7e9ee2c5ab5662
Jonathan Wilding <jbw473@outlook.com>
2026-07-01 18:42:04 -0600

    added while True loop

--- attendance_tracker.py
+++ attendance_tracker.py
@@ -1,11 +1,17 @@
 memberships = []
 
-membership_name = input("What is the members name? ").title()
-membership_bday = input("What is the members birthday? (mm/dd/yy) ")
-membership_record_number = input("What is the members membership record number? ")
-memberships.append([membership_name, membership_bday, membership_record_number])
+print("Give me the membership info for all members. type 'quit' when done. ")
+
+while True:
+    membership_name = input("What is the members name? ").title()
+    if membership_name == "Quit":
+        break
+    membership_bday = input("What is the members birthday? (mm/dd/yy) ")
+    membership_record_number = input("What is the members membership record number? ")
+    memberships.append([membership_name, membership_bday, membership_record_number])
 
 for record in memberships:
     print(record[0])
     print(record[1])
     print(record[2])
+    print()
